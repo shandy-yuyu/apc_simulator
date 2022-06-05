fa7d0bc41bba1d8e2ca401b9b4839b9968803e52
linyc0817 <f0922571727@gmail.com>
2022-06-05 23:17:18 +0800

    Add process db null test

--- src/apcService/utilities/__tests__/process.test.js
+++ src/apcService/utilities/__tests__/process.test.js
@@ -10,6 +10,31 @@ describe('test', ()=>{
         jest.resetModules();
         jest.restoreAllMocks();
     })
+    it('test db null', ()=>{
+        db.getCollection = jest.fn().mockReturnValue(null);
+        const express = require('express');
+        const mRouter = {post: jest.fn()};
+        jest.spyOn(express, 'Router').mockImplementationOnce(() => mRouter);
+        const mReq = {body: jest.fn().mockReturnThis({
+            id:0,
+            type:'SHARON',
+            thickness:0.1,
+            moisture:0.2
+        })};
+        const mSend = jest.fn().mockReturnThis(200);
+        const mRes = { 
+            status: jest.fn().mockReturnValue({send:mSend }),
+            render: jest.fn(tFactor=0.1, mFactor=0.2, metaData=null) 
+        };
+        let tmp = 0;
+        mRouter.post.mockImplementation((path, callback) => {
+            if (path === '/api/v1/process') {
+              tmp = callback(mReq, mRes);
+            }
+        });
+        require('../../routers/v1/process');
+        expect(tmp.Promise).toBeUndefined();
+    })
     it('Call by 200', ()=>{
         // global.cache = {
         //     set: jest.fn().mockReturnValueOnce(true),
@@ -29,7 +54,7 @@ describe('test', ()=>{
             thickness:0.1,
             moisture:0.2
         })};
-        const mSend = jest.fn().mockReturnThis(200);
+        const mSend = jest.fn().mockReturnThis(true);
         // Problem might happened here, but have no idea
         const mRes = { 
             status: jest.fn().mockReturnValue({send:mSend }),
@@ -42,7 +67,7 @@ describe('test', ()=>{
         });
         require('../../routers/v1/process');
         // i dont know why 500 not 200 XD
-        expect(mRes.status).toBeCalledWith(500);
+        expect(mRes.status).toBeCalledWith(200);
     })
 })
 
