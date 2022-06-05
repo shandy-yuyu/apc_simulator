2349841c85ebb097b998b7ba1214b784f0c905ec
linyc0817 <f0922571727@gmail.com>
2022-06-06 01:11:17 +0800

    Process test callback not work

--- src/apcService/utilities/__tests__/process.test.js
+++ src/apcService/utilities/__tests__/process.test.js
@@ -4,7 +4,8 @@ beforeAll(async () => {
     db.connect();
     await new Promise(r => setTimeout(r, 3000));
 });
-  
+
+// problem will be no callback 
 describe('test', ()=>{
     afterEach(()=>{
         jest.resetModules();
@@ -35,39 +36,47 @@ describe('test', ()=>{
         require('../../routers/v1/process');
         expect(tmp.Promise).toBeUndefined();
     })
+
     it('Call by 200', ()=>{
         // global.cache = {
         //     set: jest.fn().mockReturnValueOnce(true),
         //     get: jest.fn().mockReturnThis(0.5)
         // }
         db.getCollection = jest.fn().mockReturnValue({set: true, get: 0.5});
-        // db.getCollection = {
-        //     set: jest.fn().mockReturnValue(true),
-        //     get: jest.fn().mockReturnThis(0.5)
-        // }
+    //     // db.getCollection = {
+    //     //     set: jest.fn().mockReturnValue(true),
+    //     //     get: jest.fn().mockReturnThis(0.5)
+    //     // }
         const express = require('express');
         const mRouter = {post: jest.fn()};
-        jest.spyOn(express, 'Router').mockImplementationOnce(() => mRouter);
+        jest.spyOn(express, 'Router').mockImplementation(() => mRouter);
         const mReq = {body: jest.fn().mockReturnThis({
             id:0,
             type:'SHARON',
             thickness:0.1,
             moisture:0.2
         })};
-        const mSend = jest.fn().mockReturnThis(true);
+        const mSend = jest.fn().mockReturnValue(200);
         // Problem might happened here, but have no idea
         const mRes = { 
-            status: jest.fn().mockReturnValue({send:mSend }),
+            // status: jest.fn().mockReturnValue({send:mSend }),
+            status: mSend,
             render: jest.fn(tFactor=0.1, mFactor=0.2, metaData=null) 
         };
         mRouter.post.mockImplementation((path, callback) => {
             if (path === '/api/v1/process') {
+              console.log('path correct')
+              // callback didnt work 
               callback(mReq, mRes);
             }
         });
+        
         require('../../routers/v1/process');
         // i dont know why 500 not 200 XD
-        expect(mRes.status).toBeCalledWith(200);
+        // expect(mRes.status).toHaveBeenCalled();
+        expect(mReq.body).toBeCalledTimes(1);
+        expect(mRes.render).toBeCalledTimes(1);
+        expect(mRes.status).toHaveBeenCalledWith(200);
     })
 })
 
