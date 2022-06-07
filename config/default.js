47720cd434ec1e89a5a49ab729b73d655f0e1493
yinhsuan <sandy10235168@gmail.com>
2022-06-07 13:42:39 +0800

    update db url

--- config/default.js
+++ config/default.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   db: {
     // url: 'mongodb://localhost:27017/',
-    url: 'mongodb+srv://sandy:0705110490@cluster0.s6fz0.mongodb.net/?retryWrites=true&w=majority',
+    url: 'mongodb+srv://sandy:sandy0123@cluster0.s6fz0.mongodb.net/?retryWrites=true&w=majority',
     // url: process.env.DB_SERVER,
     dbName: 'apcDB',
     initValue: {
